816ebef838a1bea6a6badb81bbcb6131602278ba
bartguitar <adrielbart@gmail.com>
2026-06-26 10:11:42 -0300

    Projeto Inicial - Seção 1

---
+++ src/main/java/br/com/dio/dioprojetomodulo5curso2springdata/DioProjetoModulo5Curso2SpringDataApplication.java
@@ -0,0 +1,13 @@
+package br.com.dio.dioprojetomodulo5curso2springdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DioProjetoModulo5Curso2SpringDataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DioProjetoModulo5Curso2SpringDataApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/dio/dioprojetomodulo5curso2springdata/DioProjetoModulo5Curso2SpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.dio.dioprojetomodulo5curso2springdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DioProjetoModulo5Curso2SpringDataApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
